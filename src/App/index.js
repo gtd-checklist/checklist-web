405ca0b2cc36c134b4e1c9a3fce9c1dd0296865c
Yulya Vasilyeva <043@tut.by>
2018-10-23 21:55:36 +0300

    1) Подлкючаем глобальный стиль 2) Заменена стартовая страница на авторизацию пользователя

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 
-import { HelloWorld } from './components/Hello';
+import { GlobalStyle } from './styled';
+import { AuthorizationContainer } from './containers/AuthorizationContainer';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
-    <HelloWorld />
+    <Fragment>
+      <GlobalStyle />
+      <AuthorizationContainer />
+    </Fragment>
   </Provider>
 );
 
