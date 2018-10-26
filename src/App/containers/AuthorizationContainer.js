6b900abd2b8af098dafacbbb678e15dde625030a
Yulya Vasilyeva <043@tut.by>
2018-10-26 17:04:02 +0300

    удален компонент (был раздел на два)

--- src/App/containers/AuthorizationContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { Authorization } from '../components/Authorization';
-
-const AuthorizationContainer = () => <Authorization />;
-
-export { AuthorizationContainer };
