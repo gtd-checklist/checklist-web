b05da8ab765fa35925de42b3fe4f595959a2edff
Yulya Vasilyeva <043@tut.by>
2018-10-31 19:47:13 +0300

    Added style for h1 and renamed file

--- src/globalStyled.js
+++ src/globalStyled.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     body {
@@ -7,4 +7,10 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Source Sans Pro', sans-serif;
         font-size: 16px;
     }
+`;
+
+export const StyledMainHeader = styled.h1`
+    font-size: 2.2em;
+    letter-spacing:0.05em;
+    font-weight:normal;
 `;
