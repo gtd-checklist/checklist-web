220f1b4026b738b2950f115c863bf768755c82df
Yulya Vasilyeva <043@tut.by>
2018-11-19 18:34:32 +0300

    edit habitsData

--- src/containers/HabitsContainer.jsx
+++ src/containers/HabitsContainer.jsx
@@ -1,40 +1,9 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import { habitsData } from '../data';
 import { Habits } from '../components/Habits';
 
-const habitsData = [
-  {
-    name: 'Ранний подъем',
-    description: 'Подъем до 7 утра',
-    type: 'simple', // number
-    repeat: [1, 1, 1, 1, 1, 0, 0],
-    conditional: null, // more equally less
-    limit: null,
-    units: null,
-    done: 'true'
-  },
-  {
-    name: 'Тренировка',
-    description: 'Тренажерный зал “Пушкин” в 12:00',
-    type: 'simple', // number
-    repeat: [0, 1, 0, 1, 0, 0, 0],
-    conditional: null, // more equally less
-    limit: null,
-    units: null,
-    done: 'false'
-  },
-  {
-    name: 'Прочитать 20 страниц',
-    description: 'Книга Клиновский “Юзабилити”',
-    type: 'number', // simple
-    repeat: [1, 1, 1, 1, 1, 1, 0],
-    conditional: 'more', // equally less
-    limit: 20,
-    units: 'страниц',
-    done: 'undefined'
-  }
-];
 
 class HabitsContainer extends PureComponent {
   getHabitsItem = () => {};
